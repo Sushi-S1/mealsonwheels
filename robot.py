567c33b48906e3140bcd2d9b03bbeaf0ec18b4df
Sushil Srinatha <sushisri10@gmail.com>
2020-02-28 10:52:40 -0500

    added constructor

--- robot.py
+++ robot.py
@@ -1,11 +1,36 @@
 import pygame
 import pyserial
-import tensorflow
+#vision import stuff
+import os
+import cv2
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import tensorflow as tf
+from google.protobuf import text_format
+import argparse
+import sys
+import json
+import time
+
+screen = pygame.display.set_mode((240, 240))
+ser1 = serial.Serial("/dev/ttyACM0",115200) 
+print("connected to: " + ser1.portstr)
+ser1.flush()
+flag = False
+data = ""
 
 class Robot:
-    pygame.init()
-    ser1 = serial.Serial("/dev/ttyACM0",115200) 
-    ser2 = serial.Serial("/dev/ttyACM1",115200) 
+
+    def __init__():
+        pygame.init()
+        ser1 = serial.Serial("/dev/ttyACM0",115200) 
+        ser2 = serial.Serial("/dev/ttyACM1",115200) 
+        screen = pygame.display.set_mode((240, 240))
+        ser1.flush()
+        ser2.flush()
+        flag = False
+        data = ""
 
     def KBdrive():
         while True:
@@ -42,4 +67,3 @@ class Robot:
                     ser1.flush()
 
     def ColorSensing():
-        
