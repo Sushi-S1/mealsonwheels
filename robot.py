4780546ed8d4f7d2186b8ada7497e84f4451ec61
Sushil Srinatha <sushisri10@gmail.com>
2020-03-03 14:52:24 -0500

    added starting code for sparks

--- robot.py
+++ robot.py
@@ -13,28 +13,43 @@ import sys
 import json
 import time
 
-screen = pygame.display.set_mode((240, 240))
-ser1 = serial.Serial("/dev/ttyACM0",115200) 
-print("connected to: " + ser1.portstr)
-ser1.flush()
-flag = False
-data = ""
+
 
 class Robot:
 
     def __init__():
         pygame.init()
+        #Arduino for Drive and lift motors + CS
         ser1 = serial.Serial("/dev/ttyACM0",115200) 
+        #Arduino for Spikes
         ser2 = serial.Serial("/dev/ttyACM1",115200) 
         screen = pygame.display.set_mode((240, 240))
         ser1.flush()
         ser2.flush()
-        flag = False
-        data = ""
+        print("connected to: " + ser1.portstr + " at baudrate: " + ser1.baudrate)
+        print("connected to: " + ser2.portstr + " at baudrate: " + ser2.baudrate)
+
+        
+    def MoveForward():
+        ser1.write('F')
+
+    def MoveBackward():
+        ser1.write('B')
+
+    def MoveRight():
+        ser1.write('R')
+
+    def MoveLeft():
+        ser1.write('L')
+
+    def StopAll():
+        ser1.write('Q')
+        ser2.write('Q')
 
     def KBdrive():
-        while True:
-            for event in pygame.event.get():
+        flag = False
+        data = ""
+        for event in pygame.event.get():
                keys = key.get_pressed()
                if event.type == pygame.KEYDOWN:
                     if keys[K_UP]:
@@ -65,5 +80,11 @@ class Robot:
                 if flag == True:
                     ser1.write(data.encode())
                     ser1.flush()
+            
 
     def ColorSensing():
+    
+    def Vision():
+
+        
+
